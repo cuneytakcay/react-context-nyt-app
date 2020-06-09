d1938e585e170883b248ee0e2639b469fd76290d
cakcay <cakcay@simpleviewinc.com>
2020-06-09 14:11:56 -0700

    Created main sections

---
+++ src/header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return ( 
+        <header>
+            <div className="logo">NYT Article Search</div>
+        </header>
+    )
+}
+
+export default Header;
