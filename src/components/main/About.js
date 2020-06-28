3e1d4a2d6d5dfa046a56cde4209ceba5a2a4c57f
cakcay <cakcay@simpleviewinc.com>
2020-06-28 15:16:17 -0700

    Added and styled 404 page

--- src/components/main/About.js
+++ src/components/main/About.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const About = () => {
 	return (
-		<div className="about-container">
+		<div className="main-container">
 			<h2 style={{marginBottom: '20px'}}>About This Project</h2>
 			<p style={{maxWidth: '992px', padding: '0 16px', marginBottom: '20px'}}>
 				Using News Api to get you articles from all around the world...
--- src/components/main/NotFound.js
+++ src/components/main/NotFound.js
@@ -2,9 +2,15 @@ import React from 'react';
 
 export const NotFound = () => {
 	return (
-		<div>
-			<h2>404</h2>
-			<p>
+		<div className="main-container">
+			<h2 style={{ marginBottom: '20px' }}>404</h2>
+			<p
+				style={{
+					maxWidth: '992px',
+					padding: '0 16px',
+					marginBottom: '20px',
+				}}
+			>
 				The page you are looking for cannot be found.
 			</p>
 		</div>
