2b9bb2d2af9d5ed04379e1edae09d8ecfd031d06
cakcay <cakcay@simpleviewinc.com>
2020-07-18 23:35:07 -0700

    Modified radio buttons

--- src/components/main/form/RadioField.js
+++ src/components/main/form/RadioField.js
@@ -25,30 +25,26 @@ export const RadioField = ({ register }) => {
 		<fieldset>
 			<legend>Search for:</legend>
 			<div className="radio form-item">
-				<div>
-					<label htmlFor="radioArticles">Articles</label>
-					<input
-						type="radio"
-						name="articlesPicker"
-						id="radioArticles"
-						ref={register}
-						value="articles"
-						onChange={handleArticles}
-						checked={articles}
-					/>
-				</div>
-				<div>
-					<label htmlFor="radioBooks">Books</label>
-					<input
-						type="radio"
-						name="booksPicker"
-						id="radioBooks"
-						ref={register}
-						value="books"
-						onChange={handleBooks}
-						checked={books}
-					/>
-				</div>
+				<input
+					type="radio"
+					name="articlesPicker"
+					id="radioArticles"
+					ref={register}
+					value="articles"
+					onChange={handleArticles}
+					checked={articles}
+				/>
+				<label htmlFor="radioArticles">Articles</label>
+				<input
+					type="radio"
+					name="booksPicker"
+					id="radioBooks"
+					ref={register}
+					value="books"
+					onChange={handleBooks}
+					checked={books}
+				/>
+				<label htmlFor="radioBooks">Books</label>
 			</div>
 		</fieldset>
 	);
