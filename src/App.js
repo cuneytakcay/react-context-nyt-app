cbfddc5a86aa0c007ef87aa146f58ea5c861db84
cakcay <cuneyt.akcay@simpleviewinc.com>
2022-02-01 23:02:02 -0700

    Removing movies functions

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { Header } from './components/header/Header';
-import { Home } from './components/main/Home';
-import { About } from './components/main/About';
-import { NotFound } from './components/main/NotFound';
-import { Footer } from './components/footer/Footer';
-import './App.css';
-import ArticlesState from './context/articles/ArticlesState';
-import BooksState from './context/books/BooksState';
-import MoviesState from './context/movies/MoviesState';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { Header } from './components/header/Header'
+import { Home } from './components/main/Home'
+import { About } from './components/main/About'
+import { NotFound } from './components/main/NotFound'
+import { Footer } from './components/footer/Footer'
+import './App.css'
+import ArticlesState from './context/articles/ArticlesState'
+import BooksState from './context/books/BooksState'
 
 const App = () => {
 	return (
 		<ArticlesState>
 			<BooksState>
-				<MoviesState>
-					<Router>
-						<div className="app-container">
-							<Header />
-							<Switch>
-								<Route exact path="/" component={Home} />
-								<Route exact path="/about" component={About} />
-								<Route component={NotFound} />
-							</Switch>
-							<Footer />
-						</div>
-					</Router>
-				</MoviesState>
+				<Router>
+					<div className="app-container">
+						<Header />
+						<Switch>
+							<Route exact path="/" component={Home} />
+							<Route exact path="/about" component={About} />
+							<Route component={NotFound} />
+						</Switch>
+						<Footer />
+					</div>
+				</Router>
 			</BooksState>
 		</ArticlesState>
-	);
-};
+	)
+}
 
-export default App;
+export default App
